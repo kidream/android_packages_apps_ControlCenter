7dfaf1d46c4c241e13710b0a74584137514ce5cc
yanniks <kontakt@yanniks.de>
2013-08-25 21:52:21 +0200

    refresh button for changes and radical cleanup

--- src/com/cyandream/controlcenter/cmchanges.java
+++ src/com/cyandream/controlcenter/cmchanges.java
@@ -4,6 +4,9 @@ import com.cyandream.controlcenter.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 public class cmchanges extends Activity {
@@ -13,8 +16,34 @@ public class cmchanges extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     	mWebView = (WebView) findViewById(R.id.webview);
     	mWebView.loadUrl("http://yanniks.de/roms/cd-changes.php?device=" + android.os.Build.PRODUCT);
         mWebView.setInitialScale(110);
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.changes, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	{
+    	       switch (item.getItemId()) 
+    	        {
+    	        case android.R.id.home: 
+    	            onBackPressed();
+    	            break;
+
+    	        default:
+    	            if(item.getItemId() == R.id.refresh){
+    	                mWebView.reload();
+    	                return true;
+    	}
+    	}
+        return super.onOptionsItemSelected(item);
+    }
     }
 }
