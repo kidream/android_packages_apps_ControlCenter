a96ef86dc0d2f42d710671d6ba43fc783ac2677b
yanniks <kontakt@yanniks.de>
2013-09-19 17:15:40 +0200

    new Download Manager integration and json support
    Change-Id: I6e1f97032106d8916fab9c379fffa62ac4849b68

--- src/com/cyandream/controlcenter/updatechecker/UpdateChecker.java
+++ src/com/cyandream/controlcenter/updatechecker/UpdateChecker.java
@@ -1,42 +1,168 @@
 package com.cyandream.controlcenter.updatechecker;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.cyandream.controlcenter.R;
 import com.cyandream.controlcenter.updatechecker.splash.SplashScreen;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class UpdateChecker extends Activity {
     /** Called when the activity is first created. */
-    private TextView mTextView, mTextView2;
+    private TextView mTextView, mTextView2, sizetext;
+	private DownloadManager mgr=null;
+	private long lastDownload=-1L;;
 	DefaultHttpClient httpclient = new DefaultHttpClient();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.updatecheck);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-		Intent i = getIntent();
-		String currentversion = i.getStringExtra("currentversion");
+    	Intent i = getIntent();
+    	String currentversion = i.getStringExtra("currentversion");
+    	String size = i.getStringExtra("size");
+    	String filename = i.getStringExtra("filename");
+    	String compare = i.getStringExtra("compare");
+    	Button button = (Button) findViewById(R.id.start);
+    	String installupdate = i.getStringExtra("installupdate");
+    	if( installupdate == compare ) {
+            button.setEnabled(false);
+            }
     	mTextView = (TextView) findViewById(R.id.current);
     	mTextView2 = (TextView) findViewById(R.id.installed);
+    	sizetext = (TextView) findViewById(R.id.filesize);
         mTextView.setText(getString(R.string.current) + " " + currentversion);
+        sizetext.setText(getString(R.string.size) + " " + size);
         mTextView2.setText(getString(R.string.installed) + " " + android.os.Build.VERSION.INCREMENTAL);
         ((TextView)findViewById(R.id.installedversion)).setText(android.os.Build.VERSION.INCREMENTAL.equals(currentversion) ? getString(R.string.noupdates) : getString(R.string.updatesavalibale));
+        mgr=(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+      }
+      
+      @Override
+      public void onDestroy() {
+        super.onDestroy();
+        
+        unregisterReceiver(onComplete);
+      }
+      
+      public void startDownload() {
+        Uri uri=Uri.parse("http://yanniks.de/roms/cd-download/" + android.os.Build.PRODUCT );
+        
+        Environment
+          .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+          .mkdirs();
+        
+        lastDownload=
+          mgr.enqueue(new DownloadManager.Request(uri)
+                      .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI |
+                                              DownloadManager.Request.NETWORK_MOBILE)
+                      .setAllowedOverRoaming(false)
+                      .setTitle(getString(R.string.loadingcurrentrom))
+                      .setDescription(getString(R.string.gettingnewbuild))
+                      .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,
+                                                         "cyandream-current.zip"));
+        
+        findViewById(R.id.query).setEnabled(true);
+      }
+      
+      public void queryStatus(View v) {
+        Cursor c=mgr.query(new DownloadManager.Query().setFilterById(lastDownload));
+        
+        if (c==null) {
+        }
+        else {
+          c.moveToFirst();
+          
+          Log.d(getClass().getName(), "COLUMN_ID: "+
+                c.getLong(c.getColumnIndex(DownloadManager.COLUMN_ID)));
+          Log.d(getClass().getName(), "COLUMN_BYTES_DOWNLOADED_SO_FAR: "+
+                c.getLong(c.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR)));
+          Log.d(getClass().getName(), "COLUMN_LAST_MODIFIED_TIMESTAMP: "+
+                c.getLong(c.getColumnIndex(DownloadManager.COLUMN_LAST_MODIFIED_TIMESTAMP)));
+          Log.d(getClass().getName(), "COLUMN_LOCAL_URI: "+
+                c.getString(c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI)));
+          Log.d(getClass().getName(), "COLUMN_STATUS: "+
+                c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS)));
+          Log.d(getClass().getName(), "COLUMN_REASON: "+
+                c.getInt(c.getColumnIndex(DownloadManager.COLUMN_REASON)));
+          
+          Toast.makeText(this, statusMessage(c), Toast.LENGTH_LONG).show();
+        }
+      }
+      
+      public void viewLog(View v) {
+        startActivity(new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS));
+      }
+      
+      private String statusMessage(Cursor c) {
+        String msg="???";
+        
+        switch(c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS))) {
+          case DownloadManager.STATUS_SUCCESSFUL:
+            break;
+          
+          default:
+            break;
+        }
+        
+        return(msg);
+      }
+      
+      BroadcastReceiver onComplete=new BroadcastReceiver() {
+        public void onReceive(Context ctxt, Intent intent) {
+        	Process p;  
+            try {  
+               // Preform su to get root privledges  
+               p = Runtime.getRuntime().exec("su");   
+              
+               // Performing commands for flashing...
+               DataOutputStream os = new DataOutputStream(p.getOutputStream());  
+               os.writeBytes("mkdir -p /cache/recovery\n");  
+               os.writeBytes("echo 'boot-recovery' > /cache/recovery/command\n");  
+               os.writeBytes("echo '--update_package=/sdcard/0/Download/cyandream-current.zip' >> /cache/recovery/command\n");  
+               os.writeBytes("echo '--update_package=/sdcard/0/Download/gapps-current.zip' >> /cache/recovery/command\n");  
+               os.writeBytes("reboot recovery\n");  
+               os.flush();  
+            } catch (IOException e) {  
+                // TODO Code to run in input/output exception  
+             }  
+          findViewById(R.id.start).setEnabled(true);
+        }
+      };
 
-    }
-    	public void downloadnew (final View view) {
-        	startActivity (new Intent (this,DownloadNew.class));
-    	}
-
+      public void startdl (final View view) {
+    	delfile(view);
+    	Button button = (Button) findViewById(R.id.start);
+      	startDownload();
+        button.setEnabled(false);
+      }
+      public void delfile (final View view) {
+    	  File file = new File("/storage/emulated/legacy/Download/cyandream-current.zip");
+          @SuppressWarnings("unused")
+    		boolean deleted = file.delete();
+      }
         @Override
         public boolean onCreateOptionsMenu (Menu menu) {
             MenuInflater inflater = getMenuInflater();
--- src/com/cyandream/controlcenter/updatechecker/splash/SplashScreen.java
+++ src/com/cyandream/controlcenter/updatechecker/splash/SplashScreen.java
@@ -1,13 +1,6 @@
 package com.cyandream.controlcenter.updatechecker.splash;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.BufferedHttpEntity;
+import com.cyandream.controlcenter.lib.JsonParser;
 
 import com.cyandream.controlcenter.R;
 import com.cyandream.controlcenter.updatechecker.UpdateChecker;
@@ -16,12 +9,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SplashScreen extends Activity {
 	DefaultHttpClient httpclient = new DefaultHttpClient();
-    String line, currentversion;
+    String version, size, installupdate, filename, compare;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,39 +33,58 @@ public class SplashScreen extends Activity {
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
+			// before making http calls
+			Log.e("JSON", "Pre execute");
+
 		}
 
 		@Override
 		protected Void doInBackground(Void... arg0) {
-            try {
-            	HttpGet httppost = new HttpGet("http://yanniks.de/roms/version-" + android.os.Build.PRODUCT + ".txt");
-            	HttpResponse response = httpclient.execute(httppost);
-            	        HttpEntity ht = response.getEntity();
+			/*
+			 * Will make http call here This call will download required data
+			 * before launching the app 
+			 * example: 
+			 * 1. Downloading and storing SQLite 
+			 * 2. Downloading images 
+			 * 3. Parsing the xml / json 
+			 * 4. Sending device information to server 
+			 * 5. etc.,
+			 */
+			JsonParser jsonParser = new JsonParser();
+			String json = jsonParser
+					.getJSONFromUrl("http://yanniks.de/roms/cd-" + android.os.Build.PRODUCT + ".json" );
+			if (json != null) {
+				try {
+					JSONObject jObj = new JSONObject(json)
+							.getJSONObject("rominfo");
+					version = jObj.getString("version");
+					size = jObj.getString("size");
+					installupdate = jObj.getString("installupdate");
+					filename = jObj.getString("filename");
+					compare = jObj.getString("compare");
 
-            	        BufferedHttpEntity buf = new BufferedHttpEntity(ht);
+					Log.e("Current version: ", version + ", " + size + ", install update: " + installupdate);
 
-            	        InputStream is = buf.getContent();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
+			}
 
-            	        BufferedReader r = new BufferedReader(new InputStreamReader(is));
-
-            	        StringBuilder total = new StringBuilder();
-            	        while ((line = r.readLine()) != null) {
-            	            total.append(line);
-            	            currentversion = line;
-
-            	            }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
 			return null;
 		}
+
 
 		@Override
 		protected void onPostExecute(Void result) {
 			super.onPostExecute(result);
 			Intent i = new Intent(SplashScreen.this, UpdateChecker.class);
-			i.putExtra("currentversion", currentversion);
+			i.putExtra("currentversion", version);
+			i.putExtra("size", size);
+			i.putExtra("installupdate", installupdate);
+			i.putExtra("filename", filename);
+			i.putExtra("compare", compare);
 			startActivity(i);
 			finish();
 		}
