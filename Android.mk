af186b3e70c5ad0012dd0f1f4430aac3d8578c70
yanniks <kontakt@yanniks.de>
2013-11-04 20:31:18 +0100

    cleanup

--- Android.mk
+++ Android.mk
@@ -1,14 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-
-LOCAL_MODULE_TAGS := optional
-
 include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := ControlCenter
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PACKAGE)
 
-LOCAL_STATIC_JAVA_LIBRARIES := androidsupport
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += androidsupport:libs/android-support-v4.jar
-
-include $(BUILD_PACKAGE)
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
